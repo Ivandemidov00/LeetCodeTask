48b9d1d727c2b5653e257a7a8f0072681892af89
ivan.demidov <ivan.demidov00@mail.ru>
2022-11-21 21:47:05 +0500

    Solution

---
+++ GoalParserInterpretation_1678_Benchmark/SolutionBenchmark.cs
@@ -0,0 +1,47 @@
+using BenchmarkDotNet.Attributes;
+using GoalParserInterpretation_1678_Solution;
+using SolutionHelper;
+
+namespace GoalParserInterpretation_1678_Benchmark;
+
+[MemoryDiagnoser]
+public class SolutionBenchmark
+{
+    private ISolution<string, string> _solutionOne = null!;
+    private ISolution<string, string> _solutionTwo = null!;
+
+
+    [GlobalSetup]
+    public void GlobalSetUp()
+    {
+        _solutionOne = new SolutionOne();
+        _solutionTwo = new SolutionTwo();
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public void BenchOne(string argument)
+    {
+        _solutionOne.Resolve(argument);
+    }
+    
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public void BenchTwo(string argument)
+    {
+        _solutionTwo.Resolve(argument);
+    }
+
+    public IEnumerable<string> Data
+    {
+        get
+        {
+            yield return "G()(al)";
+            yield return "G()()";
+            yield return "G()()oal";
+            yield return "G()(y)(t)";
+            yield return "G()(al)";
+            yield return "G()(al)";
+        }
+    }
+}
---
+++ GoalParserInterpretation_1678_Solution/TestData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using NUnit.Framework;
+
+namespace GoalParserInterpretation_1678_Solution;
+
+public static class TestData
+{
+    public static IEnumerable GetData
+    {
+        get
+        {
+            yield return new TestCaseData("G()(al)").Returns("Goal");
+            yield return new TestCaseData("G()()").Returns("Goo");
+            yield return new TestCaseData("G()o").Returns("Goo");
+            yield return new TestCaseData("G(o)o").Returns("Goo");
+        }
+        
+    }
+}
